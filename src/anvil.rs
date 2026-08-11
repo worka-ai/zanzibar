e74f004ea3a8a61bbefce3087795793248645d35
zcourts <courtney.robinson@hypi.io>
2026-08-11 13:02:00 +0100

    release: qualify Zanzibar 0.3.2 with Anvil 0.7

--- src/anvil.rs
+++ src/anvil.rs
@@ -46,7 +46,7 @@ const MAX_PAGE_SIZE: u32 = 1_000;
 const ANVIL_PUBLIC_NAMESPACE: &str = "app";
 const ANVIL_PUBLIC_ID: &str = "_anvil/public";
 
-/// Durable application credentials used to connect to one Anvil 0.6 cluster.
+/// Durable application credentials used to connect to one Anvil 0.7 cluster.
 #[derive(Clone)]
 pub struct AnvilRebacConfig {
     pub endpoint: String,
